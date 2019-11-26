b8de11d7aee0a90369dde868e92447cfce0c74d8
lethekhoi <lethekhoi@gmail.com>
2019-11-26 20:27:42 +0700

    first commit

---
+++ app/src/main/java/lethekhoi/ltk/hcmus/intentexplicit/Main2Activity.java
@@ -0,0 +1,20 @@
+package lethekhoi.ltk.hcmus.intentexplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        //nhan dang string
+        Intent intent = getIntent();
+        String chuoi = intent.getStringExtra("chuoi");
+        Log.d("BBB", chuoi + "");
+    }
+}
---
+++ app/src/main/java/lethekhoi/ltk/hcmus/intentexplicit/MainActivity.java
@@ -0,0 +1,29 @@
+package lethekhoi.ltk.hcmus.intentexplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnIntentSendString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnIntentSendString = findViewById(R.id.buttonIntentString);
+        btnIntentSendString.setOnClickListener(view ->
+                {
+                    //statement
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    intent.putExtra("chuoi", "Hello main 2");
+                    startActivity(intent);
+                }
+        );
+    }
+}
